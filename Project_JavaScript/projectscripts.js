662205158e1802054ce6ccfa54bb307e9d1eb870
DougApplegate <38791739+DougApplegate79@users.noreply.github.com>
2020-12-10 00:06:13 -0500

    fixed bug in javascript

--- Project_JavaScript/projectscripts.js
+++ Project_JavaScript/projectscripts.js
@@ -1,3 +1,2 @@
-<script>src="/js/pdfobject.js"></script>
-<script>pdfobject.embed("C:\Users\DRA\OneDrive\Desktop\The Mill Code School related\themillcodeschool\Doug Applegate 2020 resume.pdf", "resume1");
-</script>
+src="/js/pdfobject.js">
+pdfobject.embed("C:\Users\DRA\OneDrive\Desktop\The Mill Code School related\themillcodeschool\Doug Applegate 2020 resume.pdf", "resume1");
